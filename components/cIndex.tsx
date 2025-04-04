ef7e17996fcdf07cc417c85c097d4ad2e9afeaee
GonzaloSepulveda <topgonzalosepulveda@gmail.com>
2025-04-04 17:05:48 +0200

    nophone

--- components/cIndex.tsx
+++ components/cIndex.tsx
@@ -1,15 +1,14 @@
-import { PageProps } from "$fresh/server.ts";
 import { DataIndex } from "../routes/index.tsx";
 
-export function cIndex(props:PageProps<DataIndex>) {
+export function cIndex(props:DataIndex) {
     return(
     <div>
       <form method = "get">
         <input type="text" placeholder="Telefono" name = "telefono"/>
         <button type="submit">Enviar</button> 
       </form>
-      <h1>Tu telefono es: {props.data.telefono}</h1>
-      <a href={"/country/"+props.data.country}>{props.data.country}</a>
+      <h1>Tu telefono es: {props.telefono}</h1>
+      <a href={"/country/"+props.country}>{props.country}</a>
     </div>
     )
 }
--- routes/nophone.tsx
+++ routes/nophone.tsx
@@ -2,10 +2,6 @@
 export default function Home() {
   return (
     <div>
-      <form method="get">
-        <input type="text" placeholder="Telefono" name="telefono" />
-        <button type="submit">Enviar</button>
-      </form>
       <h1>Tu telefono es invalido</h1>
     </div>
   );
